deb2be46de00db4a112f694ed6cfc0108f236c31
EnesUka-byte <enes2011uka@gmail.com>
2025-06-04 19:09:07 +0200

    project
    project

---
+++ Moduli26/js/main.js
@@ -0,0 +1,58 @@
+// console.log("test");
+
+
+// document.write("Welcome to Digital School")
+
+// var x = 10;
+// var _value = "Digital School"
+
+var a = 50+50;
+console.log(a);
+
+var b=20;
+var c=10;
+var d=b+c;
+
+console.log(d);
+
+var e=100;
+var f=50;
+var r=e-f;
+var sh=e*f;
+var p=e/f;
+
+console.log(r);
+console.log(sh);
+console.log(p);
+
+var n=99;
+n++;
+console.log(n);
+
+var y=101;
+n--;
+console.log(y);
+
+var x=15;
+// x+=5;
+// x-+5;
+// x/=2;
+x*=2;
+console.log(x);
+
+var v=5;
+var w=5;
+console.log(v == w);
+
+var test1=5;
+var test2=10;
+
+console.log(test1 != test2)
+
+var name="Digital";
+var name2 = "School";
+var fullname = name + " " + name2;
+
+console.log(fullname)
+console.log('Welcome to Digital ${name2}');
+
